cb625e17f0c0f7d309fe11a8dcf3cf90b0d14187
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-10 10:07:45 +0000

    rebuild and redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,15 @@ import 'react-toastify/dist/ReactToastify.css'
 
 export default function App() {
   return (
-    <AppProvider>
-      <SidebarProvider>
+    // <AppProvider>
+    <>
+      {/* <SidebarProvider>
         <ScreenProvider>
-          <SwitchProvider>
+          <SwitchProvider> */}
             <AppRoutes />
-          </SwitchProvider>
+          {/* </SwitchProvider>
         </ScreenProvider>
-      </SidebarProvider>
+      </SidebarProvider> */}
 
       {/* 2. PLACÉ À LA RACINE EN COMPOSANT AUTO-FERMANT */}
       <ToastContainer 
@@ -33,6 +34,7 @@ export default function App() {
         pauseOnHover
         theme="light"
       />
-    </AppProvider>
+      </>
+    // </AppProvider>
   )
 }
