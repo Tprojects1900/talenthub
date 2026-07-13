4eb71081642c67519c82ff2c10bea93bce047cd1
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-13 17:00:37 +0000

    alert commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ if (typeof globalThis === 'undefined') {
   window.globalThis = window;
 }
 
+alert("A");
+
 import React from "react";
-import ReactDOM from "react-dom/client"; // On revient à l'import standard
+
+alert("B");
+
+import ReactDOM from "react-dom/client";
+
+alert("C");
+
+import "./index.css";
+
+alert("D");
+
 import App from "./App";
+
+alert("E");
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
