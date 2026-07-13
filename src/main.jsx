fd268eb38592f4095457eb4c7e11e53dcd735cd7
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-13 14:24:56 +0000

    webpack fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,12 @@
-if (typeof globalThis === 'undefined') {
-  window.globalThis = window;
-}
-
 import React from "react";
-import ReactDOM from "react-dom/client"; // On revient à l'import standard
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import { HelmetProvider } from 'react-helmet-async';
+import ReactDOM from "react-dom/client";
 
-// On garde la syntaxe officielle propre à ta version de React
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </BrowserRouter>
+  <div style={{
+    color: "red",
+    fontSize: "40px",
+    padding: "40px"
+  }}>
+    REACT OK
+  </div>
 );
