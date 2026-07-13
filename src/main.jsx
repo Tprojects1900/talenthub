d032098b2aa8a7dcc0df21eae709597d27e99646
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-13 17:07:14 +0000

    alert commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,12 @@
 if (typeof globalThis === 'undefined') {
   window.globalThis = window;
 }
-
-alert("A");
-
 import React from "react";
-
-alert("B");
-
-import ReactDOM from "react-dom/client";
-
-alert("C");
-
-import "./index.css";
-
-alert("D");
-
+import ReactDOM from "react-dom/client"; // On revient à l'import standard
 import App from "./App";
-
-alert("E");
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
-alert("TOPFOOT")
 // On garde la syntaxe officielle propre à ta version de React
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
