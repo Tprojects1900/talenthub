13d94fdb2b13cb078449c46f14692358c5694ab8
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-17 09:18:34 +0000

    rebuild and show public folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -95,6 +96,15 @@ module.exports = {
 
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'public',
+                    to: '.',
+                },
+            ],
         }),
     ],
 
