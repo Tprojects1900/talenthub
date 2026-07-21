3f10e8c496da245dc55c40072225176973d05576
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-07-21 11:00:39 +0000

    message version stable

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,20 @@
-// import "./debug";
+import "./debug";
 
-// if (typeof globalThis === "undefined") {
-//     window.globalThis = window;
-// }
+if (typeof globalThis === "undefined") {
+    window.globalThis = window;
+}
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-// import "./index.css";
-// import { HelmetProvider } from "react-helmet-async";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//     <BrowserRouter>
-//         <HelmetProvider>
-//             <App />
-//         </HelmetProvider>
-//     </BrowserRouter>
-// );
-console.log("Début");
-
-document.getElementById("root").innerHTML = "<h1>Hello HTML</h1>";
-
-console.log("Fin");
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
+    </BrowserRouter>
+);
